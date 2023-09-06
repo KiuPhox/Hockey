3c9dcbad3ae6cd4367287455dec6e0cd0a43813a
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2023-09-06 14:31:01 +0700

    Init project

--- Makefile
+++ Makefile
@@ -1,13 +1,22 @@
-SOURCE = main.cpp
+SOURCE_DIR = src
+SOURCE_FILES := $(wildcard $(SOURCE_DIR)/*.cpp)
+OBJECTS := $(patsubst $(SOURCE_DIR)/%.cpp, %.o, $(SOURCE_FILES))
 EXECUTABLE = main.exe
+
+CXX = g++
+CXXFLAGS = -I$(SOURCE_DIR)/include -L$(SOURCE_DIR)/lib
+LIBS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer
 
 all: build run
 
-build:
-	g++ -I src/include -L src/lib -o main $(SOURCE) -lmingw32 -lSDL2main -lSDL2
+build: $(OBJECTS)
+	$(CXX) $(CXXFLAGS) -o $(EXECUTABLE) $(OBJECTS) $(LIBS)
+
+%.o: $(SOURCE_DIR)/%.cpp
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 run:
-	.\$(EXECUTABLE)
+	./$(EXECUTABLE)
 
 clean:
-	rm -f $(EXECUTABLE)
+	rm -f $(OBJECTS) $(EXECUTABLE)
---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+#include "RenderWindow.h"
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+        std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+    if (!(TTF_Init()))
+        std::cout << "TTF_init has failed. Error: " << SDL_GetError() << std::endl;
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    return true;
+}
+
+bool SDLinit = init();
+
+RenderWindow window("Tiny Football", 496, 800);
+
+SDL_Texture *bgTexture = window.loadTexture("assets/images/bg.png");
+
+Uint64 currentTick = SDL_GetPerformanceCounter();
+Uint64 lastTick = 0;
+double deltaTime = 0;
+
+SDL_Event event;
+
+void update()
+{
+    lastTick = currentTick;
+    currentTick = SDL_GetPerformanceCounter();
+    deltaTime = (double)((currentTick - lastTick) * 1000 / (double)SDL_GetPerformanceFrequency());
+}
+
+void render()
+{
+    window.clear();
+    window.render(0, 0, bgTexture);
+    window.display();
+}
+
+int main(int argc, char *argv[])
+{
+
+    while (true)
+    {
+        if (SDL_PollEvent(&event))
+        {
+            if (SDL_QUIT == event.type)
+            {
+                break;
+            }
+        }
+
+        update();
+        render();
+    }
+
+    window.cleanUp();
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
